8c45c0bab1dee666025f221cbecd379bc17a65d7
nitishjain07 <nitishjain4@deloitte.com>
2018-09-12 16:11:01 +0530

    server change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -15,6 +16,6 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
-app.listen('https://nitjai07.github.io/portfolio', () => {
+app.listen(PORT, () => {
   console.log('server started');
 });
