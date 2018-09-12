da5e7f9d4a92bf6d3f07e04cf2f83538c5eb8f66
nitishjain07 <nitishjain4@deloitte.com>
2018-09-12 16:07:48 +0530

    server change

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
-app.listen(3003, () => {
+app.listen('https://nitjai07.github.io/portfolio', () => {
   console.log('server started');
 });
